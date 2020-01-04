15d0df09331039e4ab83a30f2e7c4676fcb385a4
User <alokdixit10@gmail.com>
2020-01-04 23:33:04 +0530

    DBConnect 2nd Commit

--- DBconnect.py
+++ DBconnect.py
@@ -1,2 +1,4 @@
 welcome to MS SQL DB online tutorial
-This is the first session 
+This is the first session
+We will start with normalization in MS SQL
+Hi Guys ,hope you are enjoying MS SQL session
